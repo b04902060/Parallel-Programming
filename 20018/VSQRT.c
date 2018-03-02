696337a3a262b1ab150334019784e894ff521a92
Richard Chou <goodhat@Richardde-MacBook-Pro.local>
2018-03-02 22:00:02 +0800

    AC

--- 20018/VSQRT.c
+++ 20018/VSQRT.c
@@ -1,8 +1,20 @@
 #include "VSQRT.h"
-
 #include <math.h>
+#include <x86intrin.h>
+
 
 void sqrt2(float *begin, float *end) {
-     for (; begin != end; begin++)
-         *begin = sqrt(*begin);
+    int n = end - begin;
+    __m256 vec;
+
+    for (int i = 0; i < n/8; i++){
+        vec = _mm256_load_ps(begin);
+        _mm256_store_ps(begin, _mm256_sqrt_ps(vec));
+        begin+=8;
+    }
+
+    for (int i = 0; i < n%8; i++){ // deal with overload
+       *begin = sqrt(*begin);
+       begin++;
+    }
 }
--- 20018/main.c
+++ 20018/main.c
@@ -3,14 +3,14 @@
 #include <math.h>
 #include <time.h>
 #include "VSQRT.h"
- 
- 
+
+
 static float a[1048576], b[1048576];
 int main() {
     int N = 1048576;
     for (int i = 0; i < N; ++i)
         a[i] = i;
- 
+
     for (int it = 0; it < 20; it++)
     {
         memcpy(b, a, sizeof(a[0])*N);
